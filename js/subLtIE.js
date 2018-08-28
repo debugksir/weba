75a681c44370a1edd52ac0d8b20122c6fb63f8fe
debugksir <402351681@qq.com>
2018-08-28 14:58:56 +0800

    init
    init

---
+++ js/subLtIE.js
@@ -0,0 +1,13 @@
+var newbody = document.getElementById('newbody');
+newbody.innerHTML = '<div style="position: fixed; top: 148px; height: 50%; width: 100%; text-align: center; font-size: 20px; line-height: 28px;">'+
+	'<p>你使用的浏览器版本过低!</p>' + 
+	'<p>升级你的浏览器，继续访问</p>' +
+	'<div style="width: 500px; height: 50px; margin: 20px auto;">' +
+	'<a href="http://www.google.cn/intl/zh-CN/chrome/browser/" style="float: left; display:block; width:200px; height: 50px; color: #000; text-decoration: none; line-height: 50px; text-align: center; border: 1px solid #CCC;">' +
+	'<img src="../imgs/google_logo.png" width="40" height="40" style="vertical-align: middle; border-style: none; margin-right: 10px;" alt="">' + 
+	'Google'+ 
+	'</a>' + 
+	'<a href="https://www.mozilla.org/zh-CN/firefox/new/" style="float: right; display:block; width:200px; height: 50px; color: #000; text-decoration: none; line-height: 50px; text-align: center; border: 1px solid #CCC;">' +
+	'<img src="../imgs/fire_logo.png" width="40" height="40" style="vertical-align: middle; border-style: none; margin-right: 10px;" alt="">' +
+	'Firefox' +
+	'</a></div>';
